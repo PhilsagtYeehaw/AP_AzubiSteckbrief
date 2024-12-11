b95b7b6117e28b5103f105cecc476ca28932f2b6
PhilsagtYeehaw <phil.ebeling@outlook.de>
2024-12-11 10:49:43 +0100

    Start ins Projekt

---
+++ AzubiSteckbrief-Backend/src/main/java/ap/azubisteckbriefbackend/AzubiSteckbriefBackendApplication.java
@@ -0,0 +1,13 @@
+package ap.azubisteckbriefbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzubiSteckbriefBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzubiSteckbriefBackendApplication.class, args);
+    }
+
+}
---
+++ AzubiSteckbrief-Backend/src/test/java/ap/azubisteckbriefbackend/AzubiSteckbriefBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ap.azubisteckbriefbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzubiSteckbriefBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
